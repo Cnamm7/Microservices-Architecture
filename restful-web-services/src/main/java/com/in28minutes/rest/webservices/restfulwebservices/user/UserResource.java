798f41b705e27b8fcc14384589bfdca00807a798
Cnamm7 <Cna.mm7@gmail.com>
2023-09-19 18:17:17 -0400

    continue of reviewing rest api

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,38 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private UserDaoService service;
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retreiveAllUser() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User saved = service.save(user);
+        URI location = ServletUriComponentsBuilder
+                        .fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(saved.getId())
+                        .toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
