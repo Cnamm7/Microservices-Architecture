70ca8a90b7762082d62ce79ea9a526cb87e2b0c4
Cnamm7 <Cna.mm7@gmail.com>
2023-10-27 02:15:20 -0400

    creating relationship between user and the posts

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/Post.java
@@ -0,0 +1,42 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private User user;
+
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -9,3 +9,15 @@ values(10003, current_date(), 'Sina2');
 
 insert into user_details(id,birth_date,name)
 values(10004, current_date(), 'Sina3');
+
+insert into post(id,description,user_id)
+values(20001, 'I want to learn AWS', 10001);
+
+insert into post(id,description,user_id)
+values(20002, 'I want to learn Azure', 10002);
+
+insert into post(id,description,user_id)
+values(20003, 'I want to get AWS Certificate', 10001);
+
+insert into post(id,description,user_id)
+values(20004, 'I want to get Azure Certificate', 10002);
