0dcf8154757bb2a8083418dff28baf38f3dbc1d9
Cnamm7 <Cna.mm7@gmail.com>
2023-10-27 01:18:03 -0400

    exploring different Jakarta and Spring features

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into user_details(id,birth_date,name)
+values(10001, current_date(), 'Sina');
+
+insert into user_details(id,birth_date,name)
+values(10002, current_date(), 'Sina1');
+
+insert into user_details(id,birth_date,name)
+values(10003, current_date(), 'Sina2');
+
+insert into user_details(id,birth_date,name)
+values(10004, current_date(), 'Sina3');
