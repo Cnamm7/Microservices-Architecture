59ac001d2531e54b1cf86d0ec20ae37804816dcf
Cnamm7 <Cna.mm7@gmail.com>
2023-10-30 15:52:25 -0400

    appending the changes for exploring resilience4j features such as rateLimiting and bulkHead

--- building-microservice-project/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ building-microservice-project/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,5 +1,8 @@
 package com.in28minutes.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,10 +20,41 @@ public class CircuitBreakerController {
     @GetMapping("/sample-api")
     @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
     public String sampleApi() {
-        logger.info("sample Api call recieved");
+        logger.info("sample Api call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
                 String.class);
         return forEntity.getBody();
+    }
+
+    @GetMapping("/sample-api-circuit-breaker")
+    //circuit breaker basically after certain fail change the status and don't call microservice,
+    //instead it returns default answer
+    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    public String sampleApiCircuitBreaker() {
+        logger.info("sample Api call received");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+                String.class);
+        return forEntity.getBody();
+    }
+
+    @GetMapping("/sample-api-rate-limiter")
+    //rate limiter is in a certain amount of time how many calls is allowed
+    @RateLimiter(name = "default")
+    public String sampleApiRateLimiter() {
+        logger.info("sample Api call received");
+//        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+//                String.class);
+        return "sample-api";
+    }
+
+    @GetMapping("/sample-api-bulk-head")
+    // we use this again for setting specific limits for concurrent calls for instance
+    @Bulkhead(name = "sample-api-bulk-head")
+    public String sampleApiBulkHead() {
+        logger.info("sample Api call received");
+//        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+//                String.class);
+        return "sample-api";
     }
 
     public String hardcodedResponse(Exception ex) {
